serhepopovych/netconf

commit b8956c03c5acace294ef08a08a6d1ebebbc2f1a0

netconf: Introduce support for referencing existing interfaces in config
Sometimes we might want to configure settings (e.g. MTU) on existing
network interfaces like physical NICs or created earlier with netconf.

There are couple of usage scenarious for this functionality. Most
useful listed below with corresponding examples:

  1) Enslave physical NICs or veths into bridge or bonding:

     $ /netctl/etc/netconf/bond/bond0
     bond0="bond0 mtu 9600 type bond mode 802.3ad miimon 100"

     # Make sure physical interface is in DOWN state before enslave
     # as required by bonding driver.
     bond0_ref1="@en1 down master bond0"
     bond0_ref2="@en2 down master bond0"

  2) Workaround buggy veth support in iproute2 where peer
     network interface name can't be set correctly and enslave
     veths to previously created bond1:

     $ cat /netctl/etc/netconf/veth/veth10a
     # By default network interfaces created and set to UP state.
     # This is not acceptable under same circumstances (e.g. bonding
     # driver requires interface to be in DOWN state on enslave) and
     # we may need to create but keep interface DOWN.
     veth10a="veth10a down type veth peer"

     # "/netctl/bin/netconf vzup veth10a" will create interface and
     # then execute all "veth10a_ref*" statements. Put ip-link(8)
     # commands to enslave interface in "ref1".
     #
     # Note that when use interface name reference syntax (i.e. interface
     # name contains @ symbol) interface state isn't changed and left
     # in DOWN as per above veth10a="..." statement. You should bring
     # state to UP by adding corresponding statement to commandline.
     # Bonding will bring state to UP after enslaving successfuly.
     veth10a_ref1="@veth10a master bond0"

     # Here we rename veth0 -> veth10b, configure MTU and
     # bring interface UP
     veth10b="veth10b@veth0 mtu 9600 name veth10b up"

     $ cat /netctl/etc/netconf/veth/veth20a
     veth20a="veth20a down type veth peer"

netctl/lib/bash/libnetconf.sh
@@ -146,23 +146,38 @@ netconf_ifup()
 
 			u_if="${u_if%:*}"
 			ip addr replace dev "$u_if" "$@" 2>&1 |nctl_log_pipe
+			nctl_get_rc
+			# nothing to configure
+			return
+			;;
+		*@*)
+			## Existing interface (e.g. physical)
+
+			local u_if_old_name="${u_if#*@}"
+			ip link set dev "$u_if_old_name" "$@" 2>&1 |nctl_log_pipe
 			nctl_get_rc || return
+			local u_if_new_name="${u_if%@*}"
+			u_if="${u_if_new_name:-$u_if_old_name}"
 			;;
 		*)
-			## Link
+			## Non-existing interface (e.g. veth, vlan, gre, ...)
 
 			ip link replace dev "$u_if" up "$@" 2>&1 |nctl_log_pipe
 			nctl_get_rc || return
-
-			sysctl_file="/netctl/etc/netconf/sysctl.d/$u_if"
-			if [ -f "$sysctl_file" -a -s "$sysctl_file" ]; then
-				sysctl -q \
-					--pattern="^net[./]ipv[46][./](conf|neigh)[./]$u_if[./].+" \
-					--load="$sysctl_file" 2>&1 |nctl_log_pipe
-				nctl_get_rc || return
-			fi
 			;;
 	esac
+
+	# Make sure interface created
+	[ -e "$NCTL_SCN_DIR/$u_if" ] || return
+
+	# Apply sysctl(8) settings
+	local sysctl_file="/netctl/etc/netconf/sysctl.d/$u_if"
+	if [ -f "$sysctl_file" -a -s "$sysctl_file" ]; then
+		sysctl -q \
+			--pattern="^net[./]ipv[46][./](conf|neigh)[./]$u_if[./].+" \
+			--load="$sysctl_file" 2>&1 |nctl_log_pipe
+		nctl_get_rc || return
+	fi
 }
 declare -fr netconf_ifup
 
@@ -197,7 +212,12 @@ netconf_ifdown()
 		*)
 			## Link
 
-			if [ -e "$NCTL_SCN_DIR/$u_if" ]; then
+			local u_if_old_name="${u_if#*@}"
+			local u_if_new_name="${u_if%@*}"
+			u_if="${u_if_new_name:-$u_if_old_name}"
+
+			local u_if_scn="$NCTL_SCN_DIR/$u_if"
+			if [ -e "$u_if_scn" -a ! -e "$u_if_scn/device" ]; then
 				ip link del dev "$u_if" 2>&1 |nctl_log_pipe
 				nctl_get_rc || return
 			fi

netctl/bin/netconf
@@ -224,8 +224,17 @@ __netconf_action()
 					;;
 				*)
 					# Do action
-					nctl_action_for_each "netconf_$action" "$@" ||
+					local item refs addrs
+
+					for item in "$@"; do
+						eval refs="\${!${item}_ref*}"
+						# for compatibility only
+						eval addrs="\${!${item}_a*}"
+						nctl_action_for_each \
+							"netconf_$action" \
+							"$item" $refs $addrs ||
 						nctl_inc_rc rc
+					done
 					;;
 			esac
 			;;

netctl/etc/netconf/netconf.conf
@@ -192,7 +192,8 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # introducing alias to an L2 interface name (suffixed with "_a" variables).
 # Each alias represents single entry for configuring L3 information and there
 # are no limits implied by netconf on alias number (however, system might
-# place their own limits on this).
+# place their own limits on this). Note that using alias suffix (i.e. "_a")
+# now deprecated: use "_ref" suffix instead.
 #
 # L2 interface configuration performed by ip-link(8), and configuration syntax
 # has following format:
@@ -265,8 +266,8 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # L3 interface configuration performed by ip-address(8), and config syntax
 # has following format:
 #
-#   <bash_iface_name>_a<n>=['"]<linux_iface_name>:<n> ip-address(8) \
-#			       "add" options["']
+#   <bash_iface_name>_ref<n>=['"]<linux_iface_name>:<n> ip-address(8) \
+#			         "add" options["']
 #
 # where
 #   <linux_iface_name>
@@ -281,51 +282,51 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # Examples:
 # ---------
 #  # variables
-#  bond1_32_1_a0='bond1.32.1:0 10.0.0.1/30 broadcast +'
-#  eth1_32_1_a0="eth1.32.1:0 192.168.0.1/30 broadcast +"
-#  eth1_32_2_a1="eth1.32.2:1 172.16.0.1/30 broadcast +"
-#  mvl1_32_2_a0='mvl1.32.2:0 172.16.0.5/30 broadcast +'
+#  bond1_32_1_ref0='bond1.32.1:0 10.0.0.1/30 broadcast +'
+#  eth1_32_1_ref0="eth1.32.1:0 192.168.0.1/30 broadcast +"
+#  eth1_32_2_ref1="eth1.32.2:1 172.16.0.1/30 broadcast +"
+#  mvl1_32_2_ref0='mvl1.32.2:0 172.16.0.5/30 broadcast +'
 #
 #  # filenames
 #  bond1.32.1, veth1a.32.1:0, mvl1.32, mvl1.32:0, gre1, g6re1,
 #  gtp1.32.1, g6tp1.32.1
 #
 
-NETCONF_BRIDGE_REGEX='br[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_BRIDGE_REGEX_F='br[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_BRIDGE_REGEX='br[[:xdigit:]]+'
+NETCONF_BRIDGE_REGEX_F='br[[:xdigit:]]+'
 
-NETCONF_BOND_REGEX='bond[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_BOND_REGEX_F='bond[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_BOND_REGEX='bond[[:xdigit:]]+'
+NETCONF_BOND_REGEX_F='bond[[:xdigit:]]+'
 
-NETCONF_DUMMY_REGEX='(dmy|lo|null)[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_DUMMY_REGEX_F='(dmy|lo|null)[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_DUMMY_REGEX='(dmy|lo|null)[[:xdigit:]]+'
+NETCONF_DUMMY_REGEX_F='(dmy|lo|null)[[:xdigit:]]+'
 
-NETCONF_VETH_REGEX='veth[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_VETH_REGEX_F='veth[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_VETH_REGEX='veth[[:xdigit:]]+'
+NETCONF_VETH_REGEX_F='veth[[:xdigit:]]+'
 
-NETCONF_GRETAP_REGEX='gtp[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_GRETAP_REGEX_F='gtp[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_GRETAP_REGEX='gtp[[:xdigit:]]+'
+NETCONF_GRETAP_REGEX_F='gtp[[:xdigit:]]+'
 
-NETCONF_IP6GRETAP_REGEX='g6tp[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IP6GRETAP_REGEX_F='g6tp[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IP6GRETAP_REGEX='g6tp[[:xdigit:]]+'
+NETCONF_IP6GRETAP_REGEX_F='g6tp[[:xdigit:]]+'
 
-NETCONF_VXLAN_REGEX='vx[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_VXLAN_REGEX_F='vx[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_VXLAN_REGEX='vx[[:xdigit:]]+'
+NETCONF_VXLAN_REGEX_F='vx[[:xdigit:]]+'
 
-NETCONF_VLAN_REGEX='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(_[[:digit:]]{1,4})+(_a[[:digit:]]+)?'
-NETCONF_VLAN_REGEX_F='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(\.[[:digit:]]{1,4})+(:[[:digit:]]+)?'
+NETCONF_VLAN_REGEX='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(_[[:digit:]]{1,4})+'
+NETCONF_VLAN_REGEX_F='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(\.[[:digit:]]{1,4})+'
 
-NETCONF_MACVLAN_REGEX='mvl[[:xdigit:]]+(_[[:digit:]]{1,4})*(_a[[:digit:]]+)?'
-NETCONF_MACVLAN_REGEX_F='mvl[[:xdigit:]]+(\.[[:digit:]]{1,4})*(:[[:digit:]]+)?'
+NETCONF_MACVLAN_REGEX='mvl[[:xdigit:]]+(_[[:digit:]]{1,4})*'
+NETCONF_MACVLAN_REGEX_F='mvl[[:xdigit:]]+(\.[[:digit:]]{1,4})*'
 
-NETCONF_GRE_REGEX='gre[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_GRE_REGEX_F='gre[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_GRE_REGEX='gre[[:xdigit:]]+'
+NETCONF_GRE_REGEX_F='gre[[:xdigit:]]+'
 
-NETCONF_IP6GRE_REGEX='g6re[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IP6GRE_REGEX='g6re[[:xdigit:]]+'
+NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+'
 
-NETCONF_IFB_REGEX='ifb[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IFB_REGEX='ifb[[:xdigit:]]+'
+NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+'
 
 # NEIGHBOUR
 # =========
@@ -377,7 +378,7 @@ NETCONF_NEIGHBOUR_REGEX_F='(en|eth|bond|br|veth|g6?tp|vx|mvl)[[:xdigit:]]+(\.[[:
 #
 
 NETCONF_ROUTE_REGEX='[_[:alpha:]]+[_[:alnum:]]*'
-NETCONF_ROUTE_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+(\.[[:digit:]]{1,4})*(:[[:digit:]]+)?'
+NETCONF_ROUTE_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+(\.[[:digit:]]{1,4})*'
 
 # RULE
 # ====

netctl/etc/netconf/vr/.template-gateway/netconf.conf
@@ -192,7 +192,8 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # introducing alias to an L2 interface name (suffixed with "_a" variables).
 # Each alias represents single entry for configuring L3 information and there
 # are no limits implied by netconf on alias number (however, system might
-# place their own limits on this).
+# place their own limits on this). Note that using alias suffix (i.e. "_a")
+# now deprecated: use "_ref" suffix instead.
 #
 # L2 interface configuration performed by ip-link(8), and configuration syntax
 # has following format:
@@ -265,8 +266,8 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # L3 interface configuration performed by ip-address(8), and config syntax
 # has following format:
 #
-#   <bash_iface_name>_a<n>=['"]<linux_iface_name>:<n> ip-address(8) \
-#			       "add" options["']
+#   <bash_iface_name>_ref<n>=['"]<linux_iface_name>:<n> ip-address(8) \
+#			         "add" options["']
 #
 # where
 #   <linux_iface_name>
@@ -281,51 +282,51 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # Examples:
 # ---------
 #  # variables
-#  bond1_32_1_a0='bond1.32.1:0 10.0.0.1/30 broadcast +'
-#  eth1_32_1_a0="eth1.32.1:0 192.168.0.1/30 broadcast +"
-#  eth1_32_2_a1="eth1.32.2:1 172.16.0.1/30 broadcast +"
-#  mvl1_32_2_a0='mvl1.32.2:0 172.16.0.5/30 broadcast +'
+#  bond1_32_1_ref0='bond1.32.1:0 10.0.0.1/30 broadcast +'
+#  eth1_32_1_ref0="eth1.32.1:0 192.168.0.1/30 broadcast +"
+#  eth1_32_2_ref1="eth1.32.2:1 172.16.0.1/30 broadcast +"
+#  mvl1_32_2_ref0='mvl1.32.2:0 172.16.0.5/30 broadcast +'
 #
 #  # filenames
 #  bond1.32.1, veth1a.32.1:0, mvl1.32, mvl1.32:0, gre1, g6re1,
 #  gtp1.32.1, g6tp1.32.1
 #
 
-NETCONF_BRIDGE_REGEX='br[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_BRIDGE_REGEX_F='br[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_BRIDGE_REGEX='br[[:xdigit:]]+'
+NETCONF_BRIDGE_REGEX_F='br[[:xdigit:]]+'
 
-NETCONF_BOND_REGEX='bond[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_BOND_REGEX_F='bond[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_BOND_REGEX='bond[[:xdigit:]]+'
+NETCONF_BOND_REGEX_F='bond[[:xdigit:]]+'
 
-NETCONF_DUMMY_REGEX='(dmy|lo|null)[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_DUMMY_REGEX_F='(dmy|lo|null)[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_DUMMY_REGEX='(dmy|lo|null)[[:xdigit:]]+'
+NETCONF_DUMMY_REGEX_F='(dmy|lo|null)[[:xdigit:]]+'
 
-NETCONF_VETH_REGEX='veth[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_VETH_REGEX_F='veth[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_VETH_REGEX='veth[[:xdigit:]]+'
+NETCONF_VETH_REGEX_F='veth[[:xdigit:]]+'
 
-NETCONF_GRETAP_REGEX='gtp[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_GRETAP_REGEX_F='gtp[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_GRETAP_REGEX='gtp[[:xdigit:]]+'
+NETCONF_GRETAP_REGEX_F='gtp[[:xdigit:]]+'
 
-NETCONF_IP6GRETAP_REGEX='g6tp[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IP6GRETAP_REGEX_F='g6tp[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IP6GRETAP_REGEX='g6tp[[:xdigit:]]+'
+NETCONF_IP6GRETAP_REGEX_F='g6tp[[:xdigit:]]+'
 
-NETCONF_VXLAN_REGEX='vx[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_VXLAN_REGEX_F='vx[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_VXLAN_REGEX='vx[[:xdigit:]]+'
+NETCONF_VXLAN_REGEX_F='vx[[:xdigit:]]+'
 
-NETCONF_VLAN_REGEX='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(_[[:digit:]]{1,4})+(_a[[:digit:]]+)?'
-NETCONF_VLAN_REGEX_F='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(\.[[:digit:]]{1,4})+(:[[:digit:]]+)?'
+NETCONF_VLAN_REGEX='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(_[[:digit:]]{1,4})+'
+NETCONF_VLAN_REGEX_F='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(\.[[:digit:]]{1,4})+'
 
-NETCONF_MACVLAN_REGEX='mvl[[:xdigit:]]+(_[[:digit:]]{1,4})*(_a[[:digit:]]+)?'
-NETCONF_MACVLAN_REGEX_F='mvl[[:xdigit:]]+(\.[[:digit:]]{1,4})*(:[[:digit:]]+)?'
+NETCONF_MACVLAN_REGEX='mvl[[:xdigit:]]+(_[[:digit:]]{1,4})*'
+NETCONF_MACVLAN_REGEX_F='mvl[[:xdigit:]]+(\.[[:digit:]]{1,4})*'
 
-NETCONF_GRE_REGEX='gre[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_GRE_REGEX_F='gre[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_GRE_REGEX='gre[[:xdigit:]]+'
+NETCONF_GRE_REGEX_F='gre[[:xdigit:]]+'
 
-NETCONF_IP6GRE_REGEX='g6re[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IP6GRE_REGEX='g6re[[:xdigit:]]+'
+NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+'
 
-NETCONF_IFB_REGEX='ifb[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IFB_REGEX='ifb[[:xdigit:]]+'
+NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+'
 
 # NEIGHBOUR
 # =========
@@ -377,7 +378,7 @@ NETCONF_NEIGHBOUR_REGEX_F='(en|eth|bond|br|veth|g6?tp|vx|mvl)[[:xdigit:]]+(\.[[:
 #
 
 NETCONF_ROUTE_REGEX='[_[:alpha:]]+[_[:alnum:]]*'
-NETCONF_ROUTE_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+(\.[[:digit:]]{1,4})*(:[[:digit:]]+)?'
+NETCONF_ROUTE_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+(\.[[:digit:]]{1,4})*'
 
 # RULE
 # ====

netctl/etc/netconf/vr/.template-host/netconf.conf
@@ -192,7 +192,8 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # introducing alias to an L2 interface name (suffixed with "_a" variables).
 # Each alias represents single entry for configuring L3 information and there
 # are no limits implied by netconf on alias number (however, system might
-# place their own limits on this).
+# place their own limits on this). Note that using alias suffix (i.e. "_a")
+# now deprecated: use "_ref" suffix instead.
 #
 # L2 interface configuration performed by ip-link(8), and configuration syntax
 # has following format:
@@ -265,8 +266,8 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # L3 interface configuration performed by ip-address(8), and config syntax
 # has following format:
 #
-#   <bash_iface_name>_a<n>=['"]<linux_iface_name>:<n> ip-address(8) \
-#			       "add" options["']
+#   <bash_iface_name>_ref<n>=['"]<linux_iface_name>:<n> ip-address(8) \
+#			         "add" options["']
 #
 # where
 #   <linux_iface_name>
@@ -281,51 +282,51 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # Examples:
 # ---------
 #  # variables
-#  bond1_32_1_a0='bond1.32.1:0 10.0.0.1/30 broadcast +'
-#  eth1_32_1_a0="eth1.32.1:0 192.168.0.1/30 broadcast +"
-#  eth1_32_2_a1="eth1.32.2:1 172.16.0.1/30 broadcast +"
-#  mvl1_32_2_a0='mvl1.32.2:0 172.16.0.5/30 broadcast +'
+#  bond1_32_1_ref0='bond1.32.1:0 10.0.0.1/30 broadcast +'
+#  eth1_32_1_ref0="eth1.32.1:0 192.168.0.1/30 broadcast +"
+#  eth1_32_2_ref1="eth1.32.2:1 172.16.0.1/30 broadcast +"
+#  mvl1_32_2_ref0='mvl1.32.2:0 172.16.0.5/30 broadcast +'
 #
 #  # filenames
 #  bond1.32.1, veth1a.32.1:0, mvl1.32, mvl1.32:0, gre1, g6re1,
 #  gtp1.32.1, g6tp1.32.1
 #
 
-NETCONF_BRIDGE_REGEX='br[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_BRIDGE_REGEX_F='br[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_BRIDGE_REGEX='br[[:xdigit:]]+'
+NETCONF_BRIDGE_REGEX_F='br[[:xdigit:]]+'
 
-NETCONF_BOND_REGEX='bond[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_BOND_REGEX_F='bond[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_BOND_REGEX='bond[[:xdigit:]]+'
+NETCONF_BOND_REGEX_F='bond[[:xdigit:]]+'
 
-NETCONF_DUMMY_REGEX='(dmy|lo|null)[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_DUMMY_REGEX_F='(dmy|lo|null)[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_DUMMY_REGEX='(dmy|lo|null)[[:xdigit:]]+'
+NETCONF_DUMMY_REGEX_F='(dmy|lo|null)[[:xdigit:]]+'
 
-NETCONF_VETH_REGEX='veth[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_VETH_REGEX_F='veth[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_VETH_REGEX='veth[[:xdigit:]]+'
+NETCONF_VETH_REGEX_F='veth[[:xdigit:]]+'
 
-NETCONF_GRETAP_REGEX='gtp[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_GRETAP_REGEX_F='gtp[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_GRETAP_REGEX='gtp[[:xdigit:]]+'
+NETCONF_GRETAP_REGEX_F='gtp[[:xdigit:]]+'
 
-NETCONF_IP6GRETAP_REGEX='g6tp[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IP6GRETAP_REGEX_F='g6tp[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IP6GRETAP_REGEX='g6tp[[:xdigit:]]+'
+NETCONF_IP6GRETAP_REGEX_F='g6tp[[:xdigit:]]+'
 
-NETCONF_VXLAN_REGEX='vx[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_VXLAN_REGEX_F='vx[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_VXLAN_REGEX='vx[[:xdigit:]]+'
+NETCONF_VXLAN_REGEX_F='vx[[:xdigit:]]+'
 
-NETCONF_VLAN_REGEX='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(_[[:digit:]]{1,4})+(_a[[:digit:]]+)?'
-NETCONF_VLAN_REGEX_F='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(\.[[:digit:]]{1,4})+(:[[:digit:]]+)?'
+NETCONF_VLAN_REGEX='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(_[[:digit:]]{1,4})+'
+NETCONF_VLAN_REGEX_F='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(\.[[:digit:]]{1,4})+'
 
-NETCONF_MACVLAN_REGEX='mvl[[:xdigit:]]+(_[[:digit:]]{1,4})*(_a[[:digit:]]+)?'
-NETCONF_MACVLAN_REGEX_F='mvl[[:xdigit:]]+(\.[[:digit:]]{1,4})*(:[[:digit:]]+)?'
+NETCONF_MACVLAN_REGEX='mvl[[:xdigit:]]+(_[[:digit:]]{1,4})*'
+NETCONF_MACVLAN_REGEX_F='mvl[[:xdigit:]]+(\.[[:digit:]]{1,4})*'
 
-NETCONF_GRE_REGEX='gre[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_GRE_REGEX_F='gre[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_GRE_REGEX='gre[[:xdigit:]]+'
+NETCONF_GRE_REGEX_F='gre[[:xdigit:]]+'
 
-NETCONF_IP6GRE_REGEX='g6re[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IP6GRE_REGEX='g6re[[:xdigit:]]+'
+NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+'
 
-NETCONF_IFB_REGEX='ifb[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IFB_REGEX='ifb[[:xdigit:]]+'
+NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+'
 
 # NEIGHBOUR
 # =========
@@ -377,7 +378,7 @@ NETCONF_NEIGHBOUR_REGEX_F='(en|eth|bond|br|veth|g6?tp|vx|mvl)[[:xdigit:]]+(\.[[:
 #
 
 NETCONF_ROUTE_REGEX='[_[:alpha:]]+[_[:alnum:]]*'
-NETCONF_ROUTE_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+(\.[[:digit:]]{1,4})*(:[[:digit:]]+)?'
+NETCONF_ROUTE_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+(\.[[:digit:]]{1,4})*'
 
 # RULE
 # ====

netctl/etc/netconf/vr/.template-router/netconf.conf
@@ -192,7 +192,8 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # introducing alias to an L2 interface name (suffixed with "_a" variables).
 # Each alias represents single entry for configuring L3 information and there
 # are no limits implied by netconf on alias number (however, system might
-# place their own limits on this).
+# place their own limits on this). Note that using alias suffix (i.e. "_a")
+# now deprecated: use "_ref" suffix instead.
 #
 # L2 interface configuration performed by ip-link(8), and configuration syntax
 # has following format:
@@ -265,8 +266,8 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # L3 interface configuration performed by ip-address(8), and config syntax
 # has following format:
 #
-#   <bash_iface_name>_a<n>=['"]<linux_iface_name>:<n> ip-address(8) \
-#			       "add" options["']
+#   <bash_iface_name>_ref<n>=['"]<linux_iface_name>:<n> ip-address(8) \
+#			         "add" options["']
 #
 # where
 #   <linux_iface_name>
@@ -281,51 +282,51 @@ NETCONF_VR_DIR='/netctl/etc/netconf/vr'
 # Examples:
 # ---------
 #  # variables
-#  bond1_32_1_a0='bond1.32.1:0 10.0.0.1/30 broadcast +'
-#  eth1_32_1_a0="eth1.32.1:0 192.168.0.1/30 broadcast +"
-#  eth1_32_2_a1="eth1.32.2:1 172.16.0.1/30 broadcast +"
-#  mvl1_32_2_a0='mvl1.32.2:0 172.16.0.5/30 broadcast +'
+#  bond1_32_1_ref0='bond1.32.1:0 10.0.0.1/30 broadcast +'
+#  eth1_32_1_ref0="eth1.32.1:0 192.168.0.1/30 broadcast +"
+#  eth1_32_2_ref1="eth1.32.2:1 172.16.0.1/30 broadcast +"
+#  mvl1_32_2_ref0='mvl1.32.2:0 172.16.0.5/30 broadcast +'
 #
 #  # filenames
 #  bond1.32.1, veth1a.32.1:0, mvl1.32, mvl1.32:0, gre1, g6re1,
 #  gtp1.32.1, g6tp1.32.1
 #
 
-NETCONF_BRIDGE_REGEX='br[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_BRIDGE_REGEX_F='br[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_BRIDGE_REGEX='br[[:xdigit:]]+'
+NETCONF_BRIDGE_REGEX_F='br[[:xdigit:]]+'
 
-NETCONF_BOND_REGEX='bond[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_BOND_REGEX_F='bond[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_BOND_REGEX='bond[[:xdigit:]]+'
+NETCONF_BOND_REGEX_F='bond[[:xdigit:]]+'
 
-NETCONF_DUMMY_REGEX='(dmy|lo|null)[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_DUMMY_REGEX_F='(dmy|lo|null)[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_DUMMY_REGEX='(dmy|lo|null)[[:xdigit:]]+'
+NETCONF_DUMMY_REGEX_F='(dmy|lo|null)[[:xdigit:]]+'
 
-NETCONF_VETH_REGEX='veth[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_VETH_REGEX_F='veth[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_VETH_REGEX='veth[[:xdigit:]]+'
+NETCONF_VETH_REGEX_F='veth[[:xdigit:]]+'
 
-NETCONF_GRETAP_REGEX='gtp[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_GRETAP_REGEX_F='gtp[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_GRETAP_REGEX='gtp[[:xdigit:]]+'
+NETCONF_GRETAP_REGEX_F='gtp[[:xdigit:]]+'
 
-NETCONF_IP6GRETAP_REGEX='g6tp[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IP6GRETAP_REGEX_F='g6tp[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IP6GRETAP_REGEX='g6tp[[:xdigit:]]+'
+NETCONF_IP6GRETAP_REGEX_F='g6tp[[:xdigit:]]+'
 
-NETCONF_VXLAN_REGEX='vx[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_VXLAN_REGEX_F='vx[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_VXLAN_REGEX='vx[[:xdigit:]]+'
+NETCONF_VXLAN_REGEX_F='vx[[:xdigit:]]+'
 
-NETCONF_VLAN_REGEX='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(_[[:digit:]]{1,4})+(_a[[:digit:]]+)?'
-NETCONF_VLAN_REGEX_F='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(\.[[:digit:]]{1,4})+(:[[:digit:]]+)?'
+NETCONF_VLAN_REGEX='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(_[[:digit:]]{1,4})+'
+NETCONF_VLAN_REGEX_F='(en|eth|bond|br|dmy|veth|g6?tp|vx)[[:xdigit:]]+(\.[[:digit:]]{1,4})+'
 
-NETCONF_MACVLAN_REGEX='mvl[[:xdigit:]]+(_[[:digit:]]{1,4})*(_a[[:digit:]]+)?'
-NETCONF_MACVLAN_REGEX_F='mvl[[:xdigit:]]+(\.[[:digit:]]{1,4})*(:[[:digit:]]+)?'
+NETCONF_MACVLAN_REGEX='mvl[[:xdigit:]]+(_[[:digit:]]{1,4})*'
+NETCONF_MACVLAN_REGEX_F='mvl[[:xdigit:]]+(\.[[:digit:]]{1,4})*'
 
-NETCONF_GRE_REGEX='gre[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_GRE_REGEX_F='gre[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_GRE_REGEX='gre[[:xdigit:]]+'
+NETCONF_GRE_REGEX_F='gre[[:xdigit:]]+'
 
-NETCONF_IP6GRE_REGEX='g6re[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IP6GRE_REGEX='g6re[[:xdigit:]]+'
+NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+'
 
-NETCONF_IFB_REGEX='ifb[[:xdigit:]]+(_a[[:digit:]]+)?'
-NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+(:[[:digit:]]+)?'
+NETCONF_IFB_REGEX='ifb[[:xdigit:]]+'
+NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+'
 
 # NEIGHBOUR
 # =========
@@ -377,7 +378,7 @@ NETCONF_NEIGHBOUR_REGEX_F='(en|eth|bond|br|veth|g6?tp|vx|mvl)[[:xdigit:]]+(\.[[:
 #
 
 NETCONF_ROUTE_REGEX='[_[:alpha:]]+[_[:alnum:]]*'
-NETCONF_ROUTE_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+(\.[[:digit:]]{1,4})*(:[[:digit:]]+)?'
+NETCONF_ROUTE_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+(\.[[:digit:]]{1,4})*'
 
 # RULE
 # ====

commit f3ceea872e5cd4f9e1a749effa782f4cf6c4adb9

netconf: Put physical network interfacfe down on request
It is expected that on stop/down request physical network interface is
put to down state. We already check if interface is physical before
attempting to delete it but not leaving physical in UP state on stop/down
request.

Fixes: commit b8956c03c5ac ("netconf: Introduce support for ...")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -226,8 +226,12 @@ netconf_ifdown()
 			u_if="${u_if_new_name:-$u_if_old_name}"
 
 			local u_if_scn="$NCTL_SCN_DIR/$u_if"
-			if [ -e "$u_if_scn" -a ! -e "$u_if_scn/device" ]; then
-				ip link del dev "$u_if" 2>&1 |nctl_log_pipe
+			if [ -e "$u_if_scn" ]; then
+				if [ -e "$u_if_scn/device" ]; then
+					ip link set dev "$u_if" down 2>&1 |nctl_log_pipe
+				else
+					ip link del dev "$u_if" 2>&1 |nctl_log_pipe
+				fi
 			else
 				:
 			fi