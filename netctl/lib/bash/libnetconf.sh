serhepopovych/netconf

commit acbbb6d793c4a0e321c5d0c0b23927f70114545c

netconf: Move ethtool(8) settings to references
They are associated with given interface and there is little
sense to store them separately.

Now with references configuration would look like following:

  1) $ cat /netctl/etc/netconf/bond/bond0

     # create bond device
     bond0="bond0 mtu 9000 type bond mode 802.3ad miimon 100 \
     lacp_rate fast xmit_hash_policy encap2+3"

  2) $ cat /netctl/etc/netconf/phys/en1

     # move away from modern systemd names
     enp5s0f0="@enp5s0f0 down"
     enp5s0f0_ref1="@enp5s0f0 name en1"

     en1="@en1 down"

     # apply ethtool settings
     en1_ref1="en1-K gro off gso off"
     en1_ref2="en1--set-ring rx 1024 tx 1024"

     # apply sysctl settings
     en1_ref3="net/ipv4/conf/en1/rp_filter=1"

     # enslave to bond0
     en1_ref4="@en1 mtu 9000 master bond0"

     # bring interface up
     en1_ref5="@en1 up"

  3) $ cat /netctl/etc/netconf/phys/en2

     # move away from modern systemd names
     enp5s0f1="@enp5s0f1 down"
     enp5s0f1_ref1="@enp5s0f1 name en2"

     en2="@en2 down"

     # apply ethtool settings
     en2_ref1="en2-K gro off gso off"
     en2_ref2="en2--set-ring rx 1024 tx 1024"

     # apply sysctl settings
     en2_ref3="net/ipv4/conf/en2/rp_filter=1"

     # enslave to bond0
     en2_ref4="@en2 mtu 9000 master bond0"

     # bring interface up
     en2_ref5="@en2 up"

This change reverts 16e2315794fd ("netconf: Support driver and hardware
setting control via ethtool(8)").

Since ethtool(8) provides no way to suppress warnings or control verbosity
level log it's output only to file.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -160,6 +160,13 @@ netconf_ifup()
 			u_if="${u_if#*+}"
 			tc "$u_if_object" replace dev "$u_if" "$@" 2>&1 |nctl_log_pipe
 			;;
+		*-*)
+			## Ethtool settings
+
+			local u_if_object="${u_if#*-}"
+			u_if="${u_if%%-*}"
+			ethtool "-$u_if_object" "$u_if" "$@" 2>&1 |NCTL_LOG_STD=n nctl_log_pipe
+			;;
 		*=*)
 			## Sysctl settings
 
@@ -204,6 +211,9 @@ netconf_ifdown()
 			;;
 		*+*)
 			## Traffic control
+			;;
+		*-*)
+			## Ethtool settings
 			;;
 		*=*)
 			## Sysctl settings
@@ -680,70 +690,6 @@ netconf_ibusage()
 }
 
 ##
-## ETHTOOL
-##
-
-# Usage: netconf_etup <var_name>
-netconf_etup()
-{
-	# Account actions
-	trap 'netconf_account "$var_name" "$val"; trap - RETURN' RETURN
-
-	local var_name="$1"
-
-	local val
-	netconf_get_val "$var_name" val || return
-
-	eval set -- $val
-
-	ethtool "$@" 2>&1 |nctl_log_pipe
-	nctl_get_rc
-}
-
-# Usage: netconf_etdown <var_name>
-netconf_etdown()
-{
-	# Account actions
-	trap 'netconf_account "$var_name" "$val"; trap - RETURN' RETURN
-
-	local var_name="$1"
-
-	local val
-	netconf_get_val "$var_name" val || return
-
-	eval set -- $val
-
-	# No-op here
-	:
-}
-
-# Usage: netconf_etlist <var_name>
-netconf_etlist()
-{
-	# Account actions
-	trap 'netconf_account "$var_name" "$val"; trap - RETURN' RETURN
-
-	local var_name="$1"
-
-	local val
-	netconf_get_val "$var_name" val || return
-
-	set -- $val
-
-	printf '%s="%s"\n' "$var_name" "$*" 2>&1 |nctl_log_pipe
-	nctl_get_rc
-}
-
-# Usage: netconf_etusage [<action>] [<var_name_descr>]
-netconf_etusage()
-{
-	nctl_log_msg 'usage: %s %s %s...\n' \
-		"$program_invocation_short_name" \
-		"${1:-et\{up|down|list|usage\}}" \
-		"${2:-<ethtool_iface_name>}"
-}
-
-##
 ## NEIGHBOUR
 ##
 
@@ -1207,7 +1153,6 @@ netconf_vrup()
 		netconf_gre_dir="$u_netconf/gre" \
 		netconf_ip6gre_dir="$u_netconf/ip6gre" \
 		netconf_ifb_dir="$u_netconf/ifb" \
-		netconf_ethtool_dir="$u_netconf/ethtool" \
 		netconf_neighbour_dir="$u_netconf/neighbour" \
 		netconf_route_dir="$u_netconf/route" \
 		netconf_rule_dir="$u_netconf/rule" \
@@ -1279,7 +1224,6 @@ netconf_vrlist()
 		netconf_gre_dir="$u_dir/netconf/gre" \
 		netconf_ip6gre_dir="$u_dir/netconf/ip6gre" \
 		netconf_ifb_dir="$u_dir/netconf/ifb" \
-		netconf_ethtool_dir="$u_dir/netconf/ethtool" \
 		netconf_neighbour_dir="$u_dir/netconf/neighbour" \
 		netconf_route_dir="$u_dir/netconf/route" \
 		netconf_rule_dir="$u_dir/netconf/rule" \
@@ -1446,12 +1390,6 @@ netconf_source()
 				: ${netconf_ifb_regex_f:="$NETCONF_IFB_REGEX_F"}
 				: ${netconf_ifb_dir:="$NETCONF_IFB_DIR"}
 				;;
-			'ethtool')
-				netconf_ethtool_list=()
-				: ${netconf_ethtool_regex:="$NETCONF_ETHTOOL_REGEX"}
-				: ${netconf_ethtool_regex_f:="$NETCONF_ETHTOOL_REGEX_F"}
-				: ${netconf_ethtool_dir:="$NETCONF_ETHTOOL_DIR"}
-				;;
 			'neighbour')
 				netconf_neighbour_list=()
 				: ${netconf_neighbour_regex:="$NETCONF_NEIGHBOUR_REGEX"}
@@ -1597,12 +1535,6 @@ declare -a netconf_ifb_list
 declare netconf_ifb_regex
 declare netconf_ifb_regex_f
 declare netconf_ifb_dir
-
-## ETHTOOL
-declare -a netconf_ethtool_list
-declare netconf_ethtool_regex
-declare netconf_ethtool_regex_f
-declare netconf_ethtool_dir
 
 ## NEIGHBOUR
 declare -a netconf_neighbour_list

netctl/bin/netconf
@@ -32,7 +32,6 @@ declare -a crt1_request_tools_list=()
 #			 gup|gdown|glist|\
 #			 g6up|g6down|g6list|\
 #			 ibup|ibdown|iblist|\
-#			 etup|etdown|etlist|\
 #			 ngup|ngdown|nglist|\
 #			 rtup|rtdown|rtlist|\
 #			 reup|redown|relist|\
@@ -87,9 +86,6 @@ __netconf_action()
 			# Up IFB
 			"$FUNCNAME" ibup "${netconf_ifb_list[@]}" || nctl_inc_rc rc
 
-			# Up ETHTOOL
-			"$FUNCNAME" etup "${netconf_ethtool_list[@]}" || nctl_inc_rc rc
-
 			# Up NEIGHBOUR
 			"$FUNCNAME" ngup "${netconf_neighbour_list[@]}" || nctl_inc_rc rc
 
@@ -114,9 +110,6 @@ __netconf_action()
 
 			# Down NEIGHBOUR
 			"$FUNCNAME" ngdown "${netconf_neighbour_list[@]}" || nctl_inc_rc rc
-
-			# Down ETHTOOL
-			"$FUNCNAME" etdown "${netconf_ethtool_list[@]}" || nctl_inc_rc rc
 
 			# Down IFB
 			"$FUNCNAME" ibdown "${netconf_ifb_list[@]}" || nctl_inc_rc rc
@@ -210,9 +203,6 @@ __netconf_action()
 			# List IFB
 			"$FUNCNAME" iblist "${netconf_ifb_list[@]}" || nctl_inc_rc rc
 
-			# List ETHTOOL
-			"$FUNCNAME" etlist "${netconf_ethtool_list[@]}" || nctl_inc_rc rc
-
 			# List NEIGHBOUR
 			"$FUNCNAME" nglist "${netconf_neighbour_list[@]}" || nctl_inc_rc rc
 
@@ -239,7 +229,6 @@ __netconf_action()
 		gup|gdown|glist|\
 		g6up|g6down|g6list|\
 		ibup|ibdown|iblist|\
-		etup|etdown|etlist|\
 		ngup|ngdown|nglist|\
 		rtup|rtdown|rtlist|\
 		reup|redown|relist|\
@@ -287,7 +276,7 @@ usage()
 	printf >&$NCTL_STDERR \
 'usage: %s {start|stop|restart|list} [vrf|bridge|bond|physical|dummy|veth|
 	gretap|ip6gretap|vxlan|vlan|macvlan|gre|ip6gre|ifb|
-	ethtool|neighbour|route|rule|vr] ...
+	neighbour|route|rule|vr] ...
 
        %s {vfup|vfdown|vflist}    {vrf interface|help|usage} ...
 
@@ -317,8 +306,6 @@ usage()
 
        %s {ibup|ibdown|iblist}    {ifb interface|help|usage} ...
 
-       %s {etup|etdown|etlist}    {ethtool interface|help|usage} ...
-
        %s {ngup|ngdown|nglist}    {neighbour name|help|usage} ...
 
        %s {rtup|rtdown|rtlist}    {route name|help|usage} ...
@@ -327,7 +314,6 @@ usage()
 
        %s {vrup|vrdown|vrlist}    {virtual router name|help|usage} ...
 ' "$program_invocation_short_name" \
-  "$program_invocation_short_name" \
   "$program_invocation_short_name" \
   "$program_invocation_short_name" \
   "$program_invocation_short_name" \
@@ -361,7 +347,7 @@ case "$NETCONF_ACTION" in
 	start|stop|restart|list)
 		# Load VRF, BRIDGE, BOND, PHYS, DUMMY, VETH,
 		# GRETAP, IP6GRETAP, VXLAN, VLAN, MACVLAN, GRE, IP6GRE,
-		# IFB, ETHTOOL, NEIGHBOUR, ROUTE, RULE and VR configs
+		# IFB, NEIGHBOUR, ROUTE, RULE and VR configs
 		netconf_argv=("$@")
 		if nctl_is_var_of_empty netconf_argv; then
 			netconf_argv=(
@@ -379,7 +365,6 @@ case "$NETCONF_ACTION" in
 				'gre'
 				'ip6gre'
 				'ifb'
-				'ethtool'
 				'neighbour'
 				'route'
 				'rule'
@@ -443,10 +428,6 @@ case "$NETCONF_ACTION" in
 	ibup|ibdown|iblist)
 		# Load only IFB related configs
 		netconf_argv=('ifb')
-		;;
-	etup|etdown|etlist)
-		# Load only ETHTOOL related configs
-		netconf_argv=('ethtool')
 		;;
 	ngup|ngdown|nglist)
 		# Load only NEIGHBOUR related configs

netctl/etc/netconf/netconf.conf
@@ -32,7 +32,6 @@ NETCONF_MACVLAN_DIR='/netctl/etc/netconf/macvlan'
 NETCONF_GRE_DIR='/netctl/etc/netconf/gre'
 NETCONF_IP6GRE_DIR='/netctl/etc/netconf/ip6gre'
 NETCONF_IFB_DIR='/netctl/etc/netconf/ifb'
-NETCONF_ETHTOOL_DIR='/netctl/etc/netconf/ethtool'
 NETCONF_NEIGHBOUR_DIR='/netctl/etc/netconf/neighbour'
 NETCONF_ROUTE_DIR='/netctl/etc/netconf/route'
 NETCONF_RULE_DIR='/netctl/etc/netconf/rule'
@@ -358,34 +357,6 @@ NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+'
 
 NETCONF_IFB_REGEX='ifb[[:xdigit:]]+'
 NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+'
-
-# ETHTOOL
-# =======
-#
-# Control network driver and hardware settings using ethtool(8) utility could be
-# useful for example to disable flow control, disable/setup link autonegotiation
-# protocol, enable/disable various hardware offload settings like GRO, GSO, TSO,
-# etc. See ethtool(8) for more information on options that can be configured.
-#
-# Configuration is stored in "ethtool" subdirectory under netconf directory.
-# Rule configuration has following format, command line passed to ethtool(8)
-# unmodified:
-#
-#  <var_name>=['"]ethtool(8) options["']
-#
-# where
-#  <var_name>		- Valid bash(1) variable name. Typically interface name
-#			  used with not allowed for bash(1) variable name
-#			  characters replaced with '_'.
-#
-# Example:
-# --------
-#  en1="-K en1 tso off gro off gso off sg off"
-#  en2="-s en1 speed 100 duplex full autoneg off"
-#  bond1.32="-K bond1.32 rxvlan on"
-
-NETCONF_ETHTOOL_REGEX='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+[_[:alnum:]]*'
-NETCONF_ETHTOOL_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+[_[:alnum:]]*'
 
 # NEIGHBOUR
 # =========

netctl/etc/netconf/vr/.template-gateway/netconf.conf
@@ -32,7 +32,6 @@ NETCONF_MACVLAN_DIR='/netctl/etc/netconf/macvlan'
 NETCONF_GRE_DIR='/netctl/etc/netconf/gre'
 NETCONF_IP6GRE_DIR='/netctl/etc/netconf/ip6gre'
 NETCONF_IFB_DIR='/netctl/etc/netconf/ifb'
-NETCONF_ETHTOOL_DIR='/netctl/etc/netconf/ethtool'
 NETCONF_NEIGHBOUR_DIR='/netctl/etc/netconf/neighbour'
 NETCONF_ROUTE_DIR='/netctl/etc/netconf/route'
 NETCONF_RULE_DIR='/netctl/etc/netconf/rule'
@@ -358,34 +357,6 @@ NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+'
 
 NETCONF_IFB_REGEX='ifb[[:xdigit:]]+'
 NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+'
-
-# ETHTOOL
-# =======
-#
-# Control network driver and hardware settings using ethtool(8) utility could be
-# useful for example to disable flow control, disable/setup link autonegotiation
-# protocol, enable/disable various hardware offload settings like GRO, GSO, TSO,
-# etc. See ethtool(8) for more information on options that can be configured.
-#
-# Configuration is stored in "ethtool" subdirectory under netconf directory.
-# Rule configuration has following format, command line passed to ethtool(8)
-# unmodified:
-#
-#  <var_name>=['"]ethtool(8) options["']
-#
-# where
-#  <var_name>		- Valid bash(1) variable name. Typically interface name
-#			  used with not allowed for bash(1) variable name
-#			  characters replaced with '_'.
-#
-# Example:
-# --------
-#  en1="-K en1 tso off gro off gso off sg off"
-#  en2="-s en1 speed 100 duplex full autoneg off"
-#  bond1.32="-K bond1.32 rxvlan on"
-
-NETCONF_ETHTOOL_REGEX='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+[_[:alnum:]]*'
-NETCONF_ETHTOOL_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+[_[:alnum:]]*'
 
 # NEIGHBOUR
 # =========

netctl/etc/netconf/vr/.template-host/netconf.conf
@@ -32,7 +32,6 @@ NETCONF_MACVLAN_DIR='/netctl/etc/netconf/macvlan'
 NETCONF_GRE_DIR='/netctl/etc/netconf/gre'
 NETCONF_IP6GRE_DIR='/netctl/etc/netconf/ip6gre'
 NETCONF_IFB_DIR='/netctl/etc/netconf/ifb'
-NETCONF_ETHTOOL_DIR='/netctl/etc/netconf/ethtool'
 NETCONF_NEIGHBOUR_DIR='/netctl/etc/netconf/neighbour'
 NETCONF_ROUTE_DIR='/netctl/etc/netconf/route'
 NETCONF_RULE_DIR='/netctl/etc/netconf/rule'
@@ -358,34 +357,6 @@ NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+'
 
 NETCONF_IFB_REGEX='ifb[[:xdigit:]]+'
 NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+'
-
-# ETHTOOL
-# =======
-#
-# Control network driver and hardware settings using ethtool(8) utility could be
-# useful for example to disable flow control, disable/setup link autonegotiation
-# protocol, enable/disable various hardware offload settings like GRO, GSO, TSO,
-# etc. See ethtool(8) for more information on options that can be configured.
-#
-# Configuration is stored in "ethtool" subdirectory under netconf directory.
-# Rule configuration has following format, command line passed to ethtool(8)
-# unmodified:
-#
-#  <var_name>=['"]ethtool(8) options["']
-#
-# where
-#  <var_name>		- Valid bash(1) variable name. Typically interface name
-#			  used with not allowed for bash(1) variable name
-#			  characters replaced with '_'.
-#
-# Example:
-# --------
-#  en1="-K en1 tso off gro off gso off sg off"
-#  en2="-s en1 speed 100 duplex full autoneg off"
-#  bond1.32="-K bond1.32 rxvlan on"
-
-NETCONF_ETHTOOL_REGEX='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+[_[:alnum:]]*'
-NETCONF_ETHTOOL_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+[_[:alnum:]]*'
 
 # NEIGHBOUR
 # =========

netctl/etc/netconf/vr/.template-router/netconf.conf
@@ -32,7 +32,6 @@ NETCONF_MACVLAN_DIR='/netctl/etc/netconf/macvlan'
 NETCONF_GRE_DIR='/netctl/etc/netconf/gre'
 NETCONF_IP6GRE_DIR='/netctl/etc/netconf/ip6gre'
 NETCONF_IFB_DIR='/netctl/etc/netconf/ifb'
-NETCONF_ETHTOOL_DIR='/netctl/etc/netconf/ethtool'
 NETCONF_NEIGHBOUR_DIR='/netctl/etc/netconf/neighbour'
 NETCONF_ROUTE_DIR='/netctl/etc/netconf/route'
 NETCONF_RULE_DIR='/netctl/etc/netconf/rule'
@@ -358,34 +357,6 @@ NETCONF_IP6GRE_REGEX_F='g6re[[:xdigit:]]+'
 
 NETCONF_IFB_REGEX='ifb[[:xdigit:]]+'
 NETCONF_IFB_REGEX_F='ifb[[:xdigit:]]+'
-
-# ETHTOOL
-# =======
-#
-# Control network driver and hardware settings using ethtool(8) utility could be
-# useful for example to disable flow control, disable/setup link autonegotiation
-# protocol, enable/disable various hardware offload settings like GRO, GSO, TSO,
-# etc. See ethtool(8) for more information on options that can be configured.
-#
-# Configuration is stored in "ethtool" subdirectory under netconf directory.
-# Rule configuration has following format, command line passed to ethtool(8)
-# unmodified:
-#
-#  <var_name>=['"]ethtool(8) options["']
-#
-# where
-#  <var_name>		- Valid bash(1) variable name. Typically interface name
-#			  used with not allowed for bash(1) variable name
-#			  characters replaced with '_'.
-#
-# Example:
-# --------
-#  en1="-K en1 tso off gro off gso off sg off"
-#  en2="-s en1 speed 100 duplex full autoneg off"
-#  bond1.32="-K bond1.32 rxvlan on"
-
-NETCONF_ETHTOOL_REGEX='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+[_[:alnum:]]*'
-NETCONF_ETHTOOL_REGEX_F='(en|eth|bond|br|dmy|lo|veth|g6?tp|vx|mvl|g6?re)[[:xdigit:]]+[_[:alnum:]]*'
 
 # NEIGHBOUR
 # =========

commit 81af54e7033af3c4149486d52a9ecee9ea97b09b

netconf: Reorder object and interface name in ethtool(8) params string
Our generic format for references is [<new_name>|<object>]@<old_name>
while for ethtool(8) we used <new_name>-<object> that does not fit
in references concept.

Reorder fields in params string to match concept.

Before:
-------

  # apply ethtool settings
  en2_ref1="en2-K gro off gso off"
  en2_ref2="en2--set-ring rx 1024 tx 1024"

After:
------

  # apply ethtool settings
  en2_ref1="-K-en2 gro off gso off"
  en2_ref2="--set-ring-en2 rx 1024 tx 1024"

So the only sysctl(8) params are now out of references format concept but
this is less important as sysctl params string passed to sysctl(8) as is.

Fixes: acbbb6d793c4 ("netconf: Move ethtool(8) settings to references")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -198,9 +198,9 @@ netconf_ifup()
 		*-*)
 			## Ethtool settings
 
-			local u_if_object="${u_if#*-}"
-			u_if="${u_if%%-*}"
-			ethtool "-$u_if_object" "$u_if" "$@" 2>&1 |NCTL_LOG_STD=n nctl_log_pipe
+			local u_if_object="${u_if%-*}"
+			u_if="${u_if##*-}"
+			ethtool "$u_if_object" "$u_if" "$@" 2>&1 |NCTL_LOG_STD=n nctl_log_pipe
 			;;
 		*=*)
 			## Sysctl settings