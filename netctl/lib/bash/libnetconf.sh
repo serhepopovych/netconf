serhepopovych/netconf

commit 077a8d31edaba734548bdad6753d6a8f244b61ad

netconf: Relax user entry processing
Before this change variables names representing users generated
internally from second part (i.e. one after first "_") of regular
non-user variable name (i.e. vlan_USER, route_USER, etc).

Contents of user_USER variables contain all non-user variables.

Relax checks and permit "user_" prefixed variables to appear among other
variables with supported prefix in user configuration files from user/
subdirectory.

This requires to ensure duplicates are not added to netconf_user_map[]
and previous logic retained so user names still can be generated as
described above. However first occurence of direct user_USER=""
statement in user configuration file will overwrite contents of
internally generated variable with same name.

Since user (and any other netconf) configuration files source directly
into script and thus any shell statement can be used, one might access
contents of internally generated variable with simple "$" subscript.

Find more information and example configuration in "netconf help user"
output.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -1168,7 +1168,7 @@ netconf_source()
 {
 	local -a ns_files
 	local -i ns_file_idx=0 ns_var_idx=0
-	local ns_item ns_var ns_dir ns_file ns_regex ns_eval ns_tmp
+	local ns_item ns_var ns_dir ns_iter ns_regex ns_eval ns_tmp
 
 	# Need for substitution when iterating over variables names and set arguments
 	ns_eval="$IFS"
@@ -1215,13 +1215,13 @@ netconf_source()
 		ns_files=()
 		ns_file_idx=0
 
-		for ns_file in "$ns_dir"/*; do
-			[ -f "$ns_file" -a -r "$ns_file" -a -s "$ns_file" ] ||
+		for ns_iter in "$ns_dir"/*; do
+			[ -f "$ns_iter" -a -r "$ns_iter" -a -s "$ns_iter" ] ||
 				continue
 
-			. "$ns_file" || return
+			. "$ns_iter" || return
 
-			ns_files[$((ns_file_idx++))]="$ns_file"
+			ns_files[$((ns_file_idx++))]="$ns_iter"
 		done
 
 		# Skip when nothing is sourced
@@ -1230,7 +1230,7 @@ netconf_source()
 		# Source variable(s)
 		if [ "$ns_item" = 'user' ]; then
 			# |user| assumed to be the last one
-			ns_regex="${netconf_items_mtch%|user|}"
+			ns_regex="${netconf_items_mtch%|}"
 			ns_regex="${ns_regex#|}"
 		else
 			ns_regex="$ns_item"
@@ -1252,21 +1252,39 @@ netconf_source()
 				-e "s/^[[:space:]]*($ns_regex)=[\"']?[[:space:]]*[^[:space:]'\"]+.*['\"]?[[:space:]]*(#|\$)/\1/p"
 		)
 		do
-			# Note that element indexing isn't contiguous
-			eval "netconf_${ns_var%%_*}_list[\$((ns_var_idx++))]='$ns_var'"
+			ns_tmp="${ns_var%%_*}"
 
-			[ "$ns_item" = 'user' ] || continue
+			if [ "$ns_tmp" != 'user' ]; then
+				# Note that element indexing isn't contiguous
+				eval "netconf_${ns_tmp}_list[\$((ns_var_idx++))]='$ns_var'"
+
+				[ "$ns_item" = 'user' ] || continue
+
+				# Create map of user managed variables
+				netconf_user_map="$netconf_user_map$ns_var|"
+
+				ns_tmp="user_${ns_var#*_}"
+
+				# Value is a list of item(s) variable names
+				eval "$ns_tmp=\"\$${ns_tmp:+$ns_tmp }\$ns_var\""
+			else
+				ns_tmp="$ns_var"
+
+				eval "ns_var=\"\$$ns_tmp\""
+
+				IFS="$ns_eval"
+
+				for ns_iter in $ns_var; do
+					[ -z "${netconf_user_map##*|$ns_iter|*}" ] ||
+						netconf_user_map="$netconf_user_map$ns_iter|"
+				done
+
+				IFS=$'\n'
+			fi
 
 			# Use pattern match to exclude duplicates
-			ns_tmp="user_${ns_var#*_}"
 			[ -z "${netconf_user_list##*|$ns_tmp|*}" ] ||
 				netconf_user_list="$netconf_user_list$ns_tmp|"
-
-			# Create map of user managed variables
-			netconf_user_map="$netconf_user_map$ns_var|"
-
-			# Value is a list of item(s) variable names
-			eval "$ns_tmp=\"\$$ns_tmp\$ns_var \""
 		done
 
 		IFS="$ns_eval"

netctl/lib/bash/.cfg/libnetconf.cfg
@@ -1281,5 +1281,35 @@ User config can hold all items normally put to item specific directories
 (e.g. vlan/ or route/) prefixed with corresponding item prefix consolidated
 under single name (e.g. WN2019052301) to improve command line experience.
 
+It is also possible to reference existing non-user variables instead of
+overwriting them:
+
+  root$ cat >@target@/netctl/etc/netconf/user/WN2019060701 <<'EOF'
+## WN2019060701
+
+# This will add vlan_br0_1000 variable name to user_WN2019060701
+# list of items to configure before any other items so startup
+# dependency br0.1000.1 -> br0.1000 preserved.
+user_WN2019060701="vlan_br0_1000"
+
+# Append variables names "vlan_WN2019060701" and "route_WN2019060701"
+# to user_WN2019060701 variable as list of items to configure.
+vlan_WN2019060701="br0.1000.1 link br0.1000 type vlan id 1"
+vlan_WN2019060701_ref0="br0.1000.1:0 203.0.113.2/30 broadcast +"
+
+route_WN2019060701="203.0.113.4/30 via 203.0.113.1 src 203.0.113.2"
+
+# Reference neighbour entry defined outside (e.g. in neighbour/) of
+# user/ to list of items to configure. Will be added after "vlan_br0_1000"
+# and before "vlan_WN2019060701" and "route_WN2019060701" in variable
+# "user_WN2019060701" value.
+user_WN2019060701="$user_WN2019060701 neighbour_br0_1000_1"
+EOF
+
+This commonly useful in configurations where multiple users share same
+circuit (e.g. C-VLAN) and multiple C-VLANs share same S-VLAN. In that
+case circuit configuration is stored in vlan/ subdirectory and
+referenced from user/ specific configuration file like shown above.
+
 _EOF
 }

commit c24b787279838565bc3ef59cb0498e17f14a046f

libnetconf: Fix space insertion when substituding existing value
Postpone ${var:+text when exists} substitution of @ns_tmp to eval stage
to check variable stored as $ns_tmp value (i.e. user_<name>) instead of
$ns_tmp itself.

Fixes: 077a8d31edab ("netconf: Relax user entry processing")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -1268,7 +1268,7 @@ netconf_source()
 					ns_tmp="user_${ns_var#*_}"
 
 					# Value is a list of item(s) variable names
-					eval "$ns_tmp=\"\$${ns_tmp:+$ns_tmp }\$ns_var\""
+					eval "$ns_tmp=\"\${$ns_tmp:+\$$ns_tmp }\$ns_var\""
 				else
 					continue
 				fi