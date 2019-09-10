serhepopovych/netconf

commit 933577232f61311e25a3c90f37b3818404b27e0f

libnetconf: Avoid mapping same entry in user managed items map multiple times
With following content in user/ we might add item multiple times to
user map:

  root$ cat >/netctl/etc/netconf/user/WN2019061001 <<'_EOF'

  user_WN2019061001="vlan_WN2019061001"
  vlan_WN2019061001="en3.32.1 link en3.32 mtu 1500 type vlan id 1"
_EOF

When processing user items first entry processed is "user_WN2019061001"
that adds "vlan_WN2019061001" to netconf_user_map. Next internal
"user_*" variable generation code will add unconditionally
"vlan_WN2019061001" to netconf_user_map again.

Check if name already added in netconf_user_map before adding new item.

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -1261,13 +1261,15 @@ netconf_source()
 
 				[ "$ns_item" = 'user' ] || continue
 
-				# Create map of user managed variables
-				netconf_user_map="$netconf_user_map$ns_var|"
+				if [ -n "${netconf_user_map##*|$ns_var|*}" ]; then
+					# Update map of user managed variables
+					netconf_user_map="$netconf_user_map$ns_var|"
 
-				ns_tmp="user_${ns_var#*_}"
+					ns_tmp="user_${ns_var#*_}"
 
-				# Value is a list of item(s) variable names
-				eval "$ns_tmp=\"\$${ns_tmp:+$ns_tmp }\$ns_var\""
+					# Value is a list of item(s) variable names
+					eval "$ns_tmp=\"\$${ns_tmp:+$ns_tmp }\$ns_var\""
+				fi
 			else
 				ns_tmp="$ns_var"
 

commit 3703d203cd6a70a1389445fff860c2ce1f1a2675

libnetconf: Do not add "user" as username to list of users we process
This could happen with following content in user/ when "vlan_en3_32_1"
defined early by content in vlan/:

  root$ cat >/netctl/etc/netconf/user/WN2019091001 <<'_EOF'

  user_WN2019061001="vlan_en3_32_1"
  vlan_en3_32_1="en3.32.1 link en3.32 mtu 1500 type vlan id 1"
_EOF

User entry processed first, it's content(s) (i.e. "vlan_en3_32_1") added
to @netconf_user_map and later when "vlan_en3_32_1" overwrite processed
@netconf_user_list updated with "user" value stored in $ns_tmp.

Fixes: 933577232f61 ("libnetconf: Avoid mapping same entry in user managed items map multiple times")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -1269,6 +1269,8 @@ netconf_source()
 
 					# Value is a list of item(s) variable names
 					eval "$ns_tmp=\"\$${ns_tmp:+$ns_tmp }\$ns_var\""
+				else
+					continue
 				fi
 			else
 				ns_tmp="$ns_var"