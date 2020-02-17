serhepopovych/netconf

commit e3bd5c28b8459d47d4c3664b22d5aa3842842de2

libnetconf: Relax regular expression matching item variables
Formpat of variable assignment can vary to fulfil user requirements on
formatting and readability.

For example user might want to split single long to multiple shorter
lines using slash to indicate line continuation:

  br0_1000_244="br0.1000.244 link br0.1000 group downlink \
                type vlan id 244 egress-qos-map 0:0 7:5"
  br0_1000_244_ref1="net/ipv4/conf/br0.1000.244/proxy_arp=1"

  # egress
  br0_1000_244_ref110="qdisc+br0.1000.244 root handle fade: prio"
  br0_1000_244_ref111="\
      filter+br0.1000.244 \
      parent fade: \
      protocol ip \
      u32 match ip src 203.0.113.0/24 \
          match ip dst 192.0.2.1 \
      action pedit munge ip dsfield set 0xa0 pipe \
      action skbedit priority 7 \
      classid :7 \
  "

  br0_1000_244_ref112="\
      filter+br0.1000.244 \
      parent fade: \
      protocol ip \
      u32 match ip src 198.51.100.0/24 \
          match ip dst 192.0.2.1 \
      action pedit munge ip dsfield set 0xa0 pipe \
      action skbedit priority 7 \
      classid :7 \
  "

  br0_1000_244_ref115="qdisc+br0.1000.244 parent 1:1 pfifo limit 100"
  br0_1000_244_ref116="qdisc+br0.1000.244 parent 1:2 pfifo limit 100"
  br0_1000_244_ref117="qdisc+br0.1000.244 parent 1:3 pfifo limit 100"

Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -1271,7 +1271,7 @@ netconf_source()
 			xargs sed -nE \
 				-e '/^[[:space:]]*(#|$)/b' \
 				-e '/^[[:space:]]*[^[:space:]]+_(ref|a)[[:digit:]]+=/b' \
-				-e "s/^[[:space:]]*($ns_regex)=[\"']?[[:space:]]*[^[:space:]'\"]+.*['\"]?[[:space:]]*(#|\$)/\1/p" |\
+				-e "s/^[[:space:]]*($ns_regex)=[\"']?[^'\"]+/\1/p" |\
 			LC_ALL=C sort -u
 		)
 		do

commit cc9efb080d910a6f539c7dd795dcda406471a68f

netconf: Fix variable name=value regular expression
Value might be on single line and enclosed within ' or " quotes.

Fixes: e3bd5c28b845 ("libnetconf: Relax regular expression matching item variables")
Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

netctl/lib/bash/libnetconf.sh
@@ -1271,7 +1271,7 @@ netconf_source()
 			xargs sed -nE \
 				-e '/^[[:space:]]*(#|$)/b' \
 				-e '/^[[:space:]]*[^[:space:]]+_(ref|a)[[:digit:]]+=/b' \
-				-e "s/^[[:space:]]*($ns_regex)=[\"']?[^'\"]+/\1/p" |\
+				-e "s/^[[:space:]]*($ns_regex)=[\"']?[^'\"]+['\"]?/\1/p" |\
 			LC_ALL=C sort -u
 		)
 		do